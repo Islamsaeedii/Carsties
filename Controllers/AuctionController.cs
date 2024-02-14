6704fe73287e1509ccb7c75f3bbdb830e00aecd5
Islam Saeed Khodary <98616151+Islamsaeedii@users.noreply.github.com>
2024-02-14 05:41:08 +0200

    Update AuctionController.cs

--- Controllers/AuctionController.cs
+++ Controllers/AuctionController.cs
@@ -9,55 +9,47 @@ namespace AuctionService.Controllers
 {
     [ApiController]
     [Route("api/auctions")]
-    public class AuctionController : ControllerBase
+    public class AuctionController(AuctionDbContext context, IMapper mapper) : ControllerBase
     {
-        private readonly AuctionDbContext _context;
-        private readonly IMapper _mapper;
-
-        public AuctionController(AuctionDbContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
         [HttpGet]
         public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
         {
-            var auctions = await _context.Auctions
+            var auctions = await context.Auctions
                                 .Include(a => a.Item)
                                 .OrderBy(x => x.Item.Make)
                                 .ToListAsync();
 
-            return _mapper.Map<List<AuctionDto>>(auctions);
+            return mapper.Map<List<AuctionDto>>(auctions);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
         {
-            var auction = await _context.Auctions
+            var auction = await context.Auctions
                                 .Include(a => a.Item)
                                 .FirstOrDefaultAsync(x => x.Id == id);
             if (auction == null) return NotFound();
 
-            return _mapper.Map<AuctionDto>(auction);
+            return mapper.Map<AuctionDto>(auction);
         }
         [HttpPost]
         public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
         {
-            var auction = _mapper.Map<Auction>(createAuctionDto);
+            var auction = mapper.Map<Auction>(createAuctionDto);
             //TODO: add current user as seller
             auction.Seller = "test";
 
-            _context.Auctions.Add(auction);
+            context.Auctions.Add(auction);
 
-            var result = await _context.SaveChangesAsync() > 0;
+            var result = await context.SaveChangesAsync() > 0;
             if (!result) return BadRequest("Could not save changes to the DB");
 
-            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, mapper.Map<AuctionDto>(auction));
         }
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
         {
-            var auction = await _context.Auctions
+            var auction = await context.Auctions
                          .Include(x => x.Item)
                          .FirstOrDefaultAsync(x => x.Id == id);
 
@@ -68,18 +60,18 @@ namespace AuctionService.Controllers
             auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
             auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
             auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
-            var result = await _context.SaveChangesAsync() > 0;
+            var result = await context.SaveChangesAsync() > 0;
             if (!result) return BadRequest("Problem saving changes.");
             return Ok();
         }
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteAuction(Guid id)
         {
-            var auction = await _context.Auctions.FindAsync(id);
+            var auction = await context.Auctions.FindAsync(id);
             if (auction == null) return NotFound();
             // TODO: check seller == username
-            _context.Remove(auction);
-            var result = await _context.SaveChangesAsync() > 0;
+            context.Remove(auction);
+            var result = await context.SaveChangesAsync() > 0;
             if (!result) return BadRequest("Could not update DB.");
             return Ok();
         }
